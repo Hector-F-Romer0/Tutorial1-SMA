5d7698e18a9066962bf05c3c0d6d14efa01b1525
Hector-f-Romero <hector.f.romero28@gmail.com>
2024-08-03 10:54:17 -0500

    feat: complete first Jest tests

---
+++ src/avatar/domain/Avatar.ts
@@ -0,0 +1,19 @@
+export class Avatar {
+	private horizontalPosition: number;
+
+	constructor() {
+		this.horizontalPosition = 0;
+	}
+
+	getHorizontalPosition(): number {
+		return this.horizontalPosition;
+	}
+
+	moveLeft() {
+		this.horizontalPosition--;
+	}
+
+	moveRight() {
+		this.horizontalPosition++;
+	}
+}
